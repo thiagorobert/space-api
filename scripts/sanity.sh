4488a410d1a12d920ee9ca65d28a10002a96700c
thiago <thiagorobert@gmail.com>
2022-01-21 21:35:19 -0800

    Make the UI functional: the TLE entered in the textarea will be the input to API 'ToOrbit'.

--- scripts/sanity.sh
+++ scripts/sanity.sh
@@ -2,36 +2,61 @@
 
 set -e
 
-echo "Testing space_api_proto.Tle/Decode"
+HOSTNAME="${1:-localhost}"
+GRPCURL_PLAINTEXT="--plaintext"
+if [[ ${HOSTNAME} != "localhost" ]]; then
+  GRPCURL_PLAINTEXT=""
+fi
+REST_ENDPOINT=${HOSTNAME}:8081
+if [[ ${HOSTNAME} != "localhost" ]]; then
+  REST_ENDPOINT=https://${HOSTNAME}
+fi
+
+echo "+++++++ Testing hostname ${HOSTNAME} +++++++"
+
+
+echo "==== Testing space_api_proto.Tle/Decode ===="
 echo
-echo "gRPC"
+echo "*********** gRPC"
 grpcurl \
-  --plaintext \
+  ${GRPCURL_PLAINTEXT} \
   -d '{"tle_data": {"name": "ISS (ZARYA)", "line1": "1 25544U 98067A   98324.28472222 -.00003657  11563-4  00000+0 0  9996", "line2": "2 25544 051.5908 168.3788 0125362 086.4185 359.7454 16.05064833    05"}}' \
-  localhost:9090 \
+  ${HOSTNAME}:9090 \
   space_api_proto.Tle/Decode
 
 echo
-echo "REST"
-curl -X POST localhost:8081/space/v1/tle/decode \
+echo "*********** REST"
+curl -s -X POST ${REST_ENDPOINT}/space/v1/tle/decode \
   -d '{"tle_data": {"name": "ISS (ZARYA)", "line1": "1 25544U 98067A   98324.28472222 -.00003657  11563-4  00000+0 0  9996", "line2": "2 25544 051.5908 168.3788 0125362 086.4185 359.7454 16.05064833    05"}}'
 
 
-echo "Testing space_api_proto.Tle/ToOrbit"
+echo "==== Testing space_api_proto.Tle/ToOrbit ===="
 echo
-echo "gRPC"
+echo "*********** gRPC"
 grpcurl \
-  --plaintext \
+  ${GRPCURL_PLAINTEXT} \
   -d '{"tle_data": {"name": "ISS (ZARYA)", "line1": "1 25544U 98067A   98324.28472222 -.00003657  11563-4  00000+0 0  9996", "line2": "2 25544 051.5908 168.3788 0125362 086.4185 359.7454 16.05064833    05"}}' \
-  localhost:9090 \
+  ${HOSTNAME}:9090 \
   space_api_proto.Tle/ToOrbit
 
 echo
-echo "REST"
-curl -X POST localhost:8081/space/v1/tle/orbit \
+echo "*********** REST"
+curl -s -X POST ${REST_ENDPOINT}/space/v1/tle/orbit \
   -d '{"tle_data": {"name": "ISS (ZARYA)", "line1": "1 25544U 98067A   98324.28472222 -.00003657  11563-4  00000+0 0  9996", "line2": "2 25544 051.5908 168.3788 0125362 086.4185 359.7454 16.05064833    05"}}'
 
 
-echo "Testing UI"
+echo "==== Testing UI ===="
 echo
-curl -X GET localhost:8080
+echo "*********** Landing page"
+curl -s -X GET ${HOSTNAME}:8080
+
+echo
+echo "*********** Landing page post"
+curl -s -X POST ${HOSTNAME}:8080/submit \
+  -d '-----------------------------23605125921461961015802936950
+Content-Disposition: form-data; name="tle"
+
+ISS (ZARYA)
+1 25544U 98067A   98324.28472222 -.00003657  11563-4  00000+0 0  9996
+2 25544 051.5908 168.3788 0125362 086.4185 359.7454 16.05064833    05
+-----------------------------23605125921461961015802936950--'
--- src/ui/app.js
+++ src/ui/app.js
@@ -1,35 +1,41 @@
 const http = require('http')
 const https = require('https')
 const static = require('node-static')
+const decodeHTML = require('decode-html')
 
 const hostname = '0.0.0.0'
 const port = 8080
 
-const data = new TextEncoder().encode(
-    JSON.stringify({
-      tle_data: {
-        name: "ISS (ZARYA)",
-        line1: "1 25544U 98067A   98324.28472222 -.00003657  11563-4  00000+0 0  9996",
-        line2: "2 25544 051.5908 168.3788 0125362 086.4185 359.7454 16.05064833    05"
-      }
-    })
-)
+function tlePartsToEncodedJson(name, line1, line2) {
+  return new TextEncoder().encode(
+      JSON.stringify({
+        tle_data: {
+          name: name,
+          line1: line1,
+          line2: line2
+        }
+      }))
+}
 
-const staticFiles = new static.Server(__dirname, { cache: 0 })
-
-const options = {
-  hostname: 'api.thiago.pub',
-  port: 443,
-  path: '/space/v1/tle/orbit',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Content-Length': data.length
+function orbitRequestOpts(dataLen) {
+  return {
+    hostname: 'api.thiago.pub',
+    port: 443,
+    path: '/space/v1/tle/orbit',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': dataLen
+    }
   }
 }
 
-function orbit(out) {
-  const req = https.request(options, res => {
+const staticFiles = new static.Server(__dirname, { cache: 0 })
+
+function orbit(name, line1, line2, out) {
+  const reqBody = tlePartsToEncodedJson(name, line1, line2)
+  const opts = orbitRequestOpts(reqBody.length)
+  const req = https.request(opts, res => {
     res.on('data', d => {
       out.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'})
       out.end(d.toString())
@@ -40,7 +46,7 @@ function orbit(out) {
     console.error(error)
   })
 
-  req.write(data)
+  req.write(reqBody)
   req.end()
 }
 
@@ -48,13 +54,35 @@ const server = http.createServer((req, res) => {
   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
   res.setHeader('Pragma', 'no-cache')
 
-  if (req.url == '/submit') {
-    orbit(res)
+  if (req.url === '/submit' && req.method === 'POST') {
+    let body = ''
+    req.on('data', chunk => {
+      body += chunk.toString(); // convert Buffer to string
+    })
+    req.on('end', () => {
+      /* This is pretty ludicrous, but it's the best I could get to.
+
+       Form posts with Content-Type 'multipart/form-data', so body looks like
+       -----------------------------23605125921461961015802936950
+       Content-Disposition: form-data; name="tle"
+
+       ISS (ZARYA)
+       1 25544U 98067A   98324.28472222 -.00003657  11563-4  00000+0 0  9996
+       2 25544 051.5908 168.3788 0125362 086.4185 359.7454 16.05064833    05
+       -----------------------------23605125921461961015802936950--
+
+       I didn't find a reasonable parser for that, so I simply retrieve the lines
+       I need from the body (lines 3 to 5) and trim them to remove the carriage return
+       characters.
+       */
+      const bodyParts = body.split('\n')
+      orbit(bodyParts[3].trim(), bodyParts[4].trim(), bodyParts[5].trim(), res)
+    })
   } else {
     staticFiles.serve(req, res)
   }
-});
+})
 
 server.listen(port, hostname, () => {
   console.log(`UI server running on http://${hostname}:${port}/`)
-});
+})
