656d6bf1f4dd0cffb7fabdf0bb0898b36f7846a6
thiago <thiagorobert@gmail.com>
2022-01-25 15:17:07 -0800

    Finish implementation of the UI. It now gives the user decoded TLE, orbit string, and orbit visualization for the input TLE.

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -10,8 +10,16 @@ if [ ${CLEAN} != "noclean" ]; then
 fi
 
 python src/python_grpc/server.py &
+GRPC_SERVER_PID=$!
 ./rest_reverse_proxy &
+REST_PROXY_PID=$!
 node src/ui/app.js &
+NODE_UI_PID=$!
 
-# Kill background processes.
-trap 'trap - TERM && kill -- -$$' INT TERM EXIT
+echo "Background processes started: $GRPC_SERVER_PID $REST_PROXY_PID $NODE_UI_PID"
+
+# Kill background processes on script termination.
+trap "kill -9 $GRPC_SERVER_PID $REST_PROXY_PID $NODE_UI_PID" INT TERM EXIT
+
+# Timeout in 5 minutes.
+sleep 300
--- src/ui/app.js
+++ src/ui/app.js
@@ -1,61 +1,99 @@
 const http = require('http')
 const https = require('https')
-const static = require('node-static')
+const fs = require('fs');
 const space = require('space-client-js')
+const static = require('node-static')
+const mustache = require('mustache')
 
 const hostname = '0.0.0.0'
 const port = 8080
 const staticFiles = new static.Server(__dirname, { cache: 0 })
-const spaceApi = new space.TleApi(
-    new space.ApiClient(basePath='https://api.thiago.pub'))
+const spaceApi = new space.TleApi(new space.ApiClient(basePath='https://api.thiago.pub'))
 
-function orbit(name, line1, line2, out) {
-  var tleOrbitReq = new space.TleToOrbitReq()
-  tleOrbitReq.tleData = new space.TleData()
-  tleOrbitReq.tleData.name = name
-  tleOrbitReq.tleData.line1 = line1
-  tleOrbitReq.tleData.line2 = line2
-  var callback = function(error, data, response) {
+let template
+fs.readFile(__dirname + '/index.html', 'utf-8', function (err, data) {
+  if (err) {
+    return console.error(err)
+  }
+  template = data
+})
+
+function bodyToTleData(body) {
+  const tleData = new space.TleData()
+  /* This is pretty ludicrous, but it's the best I could get to.
+
+   Form posts with Content-Type 'multipart/form-data', so body looks like
+   -----------------------------23605125921461961015802936950
+   Content-Disposition: form-data; name="tle"
+
+   ISS (ZARYA)
+   1 25544U 98067A   98324.28472222 -.00003657  11563-4  00000+0 0  9996
+   2 25544 051.5908 168.3788 0125362 086.4185 359.7454 16.05064833    05
+   -----------------------------23605125921461961015802936950--
+
+   I didn't find a reasonable parser for that, so I simply retrieve the lines
+   I need from the body (lines 3 to 5) and trim them to remove the carriage return
+   characters.
+   */
+  const bodyParts = body.split('\n')
+  tleData.name = bodyParts[3].trim().trimLeft()
+  tleData.line1 = bodyParts[4].trim().trimLeft()
+  tleData.line2 = bodyParts[5].trim().trimLeft()
+  return tleData
+}
+
+function decodedToHtml(decoded) {
+  let out = decoded.replace('TLE(', '<br /><b>')
+  out = out.replace(/=/g, ':</b>&nbsp;')
+  return '<b>TLE:</b>' + out.replace(/, /g, '<br /><b>').slice(0, -1)
+}
+
+function process(tleData, out) {
+  const decodeReq = new space.TleToOrbitReq()
+  decodeReq.tleData = tleData
+  let decoded = ''
+  spaceApi.tleDecode(decodeReq, function(error, data, response) {
     if (error) {
       console.error(error)
     } else {
-      out.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'})
-      out.end(data.orbit)
+      decoded = data.decoded
     }
-  };
-  spaceApi.tleToOrbit(tleOrbitReq, callback)
+  })
+
+  const toOrbitReq = new space.TleToOrbitReq()
+  toOrbitReq.tleData = tleData
+  spaceApi.tleToOrbit(toOrbitReq, function(error, data, response) {
+    if (error) {
+      console.error(error)
+    } else {
+      out.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+      const rendered = mustache.render(template,
+          {
+            tle: [tleData.name, tleData.line1, tleData.line2].join('\n'),
+            decoded: decodedToHtml(decoded),
+            orbit: '<b>Orbit:</b> ' + data.orbit,
+            visualization: '<iframe class="orbit-visualization" src="http://localhost:9091"></iframe>'
+          })
+      out.end(rendered)
+    }
+  })
 }
 
 const server = http.createServer((req, res) => {
   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
   res.setHeader('Pragma', 'no-cache')
 
-  if (req.url === '/submit' && req.method === 'POST') {
+  if (req.url === '/' && req.method === 'GET') {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+    const rendered = mustache.render(template, {})
+    res.end(rendered)
+  } else if (req.url === '/submit' && req.method === 'POST') {
     let body = ''
     req.on('data', chunk => {
-      body += chunk.toString(); // convert Buffer to string
+      body += chunk.toString()
     })
     req.on('end', () => {
-      /* This is pretty ludicrous, but it's the best I could get to.
-
-       Form posts with Content-Type 'multipart/form-data', so body looks like
-       -----------------------------23605125921461961015802936950
-       Content-Disposition: form-data; name="tle"
-
-       ISS (ZARYA)
-       1 25544U 98067A   98324.28472222 -.00003657  11563-4  00000+0 0  9996
-       2 25544 051.5908 168.3788 0125362 086.4185 359.7454 16.05064833    05
-       -----------------------------23605125921461961015802936950--
-
-       I didn't find a reasonable parser for that, so I simply retrieve the lines
-       I need from the body (lines 3 to 5) and trim them to remove the carriage return
-       characters.
-       */
-      const bodyParts = body.split('\n')
-      const name = bodyParts[3].trim().trimLeft()
-      const line1 = bodyParts[4].trim().trimLeft()
-      const line2 = bodyParts[5].trim().trimLeft()
-      orbit(name, line1, line2, res)
+      process(bodyToTleData(body), res)
     })
   } else {
     staticFiles.serve(req, res)
