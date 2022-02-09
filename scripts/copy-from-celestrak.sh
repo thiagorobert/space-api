9e2eb2dcfe457c6302dbf463604d0d543912986f
thiago <thiagorobert@gmail.com>
2022-02-09 15:32:10 -0800

    Bug fix: copy-from-celestrak.sh script should run indefinitely, copying files daily.

--- scripts/copy-from-celestrak.sh
+++ scripts/copy-from-celestrak.sh
@@ -2,11 +2,13 @@
 
 set -e
 
-SOCRATES_LATEST=${CODE_ROOT}/celestrak/socrates-`date +"%Y%m%d%H%M%S"`.txt
-curl -s https://celestrak.com/SOCRATES/sort-timein.txt -o ${SOCRATES_LATEST}
-ln -s ${SOCRATES_LATEST} ${CODE_ROOT}/celestrak/socrates-latest.txt
-STATIONS_LATEST=${CODE_ROOT}/celestrak/stations-`date +"%Y%m%d%H%M%S"`.txt
-curl -s https://celestrak.com/NORAD/elements/stations.txt -o ${STATIONS_LATEST}
-ln -s ${STATIONS_LATEST} ${CODE_ROOT}/celestrak/stations-latest.txt
+while true; do
+  SOCRATES_LATEST=${CODE_ROOT}/celestrak/socrates-`date +"%Y%m%d%H%M%S"`.txt
+  curl -s https://celestrak.com/SOCRATES/sort-timein.txt -o ${SOCRATES_LATEST}
+  ln -s ${SOCRATES_LATEST} ${CODE_ROOT}/celestrak/socrates-latest.txt
+  STATIONS_LATEST=${CODE_ROOT}/celestrak/stations-`date +"%Y%m%d%H%M%S"`.txt
+  curl -s https://celestrak.com/NORAD/elements/stations.txt -o ${STATIONS_LATEST}
+  ln -s ${STATIONS_LATEST} ${CODE_ROOT}/celestrak/stations-latest.txt
 
-sleep 86400
+  sleep 86400
+done
