22ee440574552f85583fe5e2974f6f04332c81cf
thiago <thiagorobert@gmail.com>
2022-02-14 08:40:34 -0800

    Fix script to copy data from Celestrak; it was failing since symlinks already existed.

--- scripts/copy-from-celestrak.sh
+++ scripts/copy-from-celestrak.sh
@@ -5,9 +5,11 @@ set -e
 while true; do
   SOCRATES_LATEST=${CODE_ROOT}/celestrak/socrates-`date +"%Y%m%d%H%M%S"`.txt
   curl -s https://celestrak.com/SOCRATES/sort-timein.txt -o ${SOCRATES_LATEST}
+  rm ${CODE_ROOT}/celestrak/socrates-latest.txt
   ln -s ${SOCRATES_LATEST} ${CODE_ROOT}/celestrak/socrates-latest.txt
   STATIONS_LATEST=${CODE_ROOT}/celestrak/stations-`date +"%Y%m%d%H%M%S"`.txt
   curl -s https://celestrak.com/NORAD/elements/stations.txt -o ${STATIONS_LATEST}
+  rm ${CODE_ROOT}/celestrak/stations-latest.txt
   ln -s ${STATIONS_LATEST} ${CODE_ROOT}/celestrak/stations-latest.txt
 
   sleep 86400
